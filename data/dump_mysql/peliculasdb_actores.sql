cf4fc6614d0e33e272a47e62aee033ccd3edf8f2
Hanxco <nachohitadg@gmail.com>
2022-01-30 02:42:16 +0100

    add csv and dump files sql

---
+++ data/dump_mysql/peliculasdb_actores.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: peliculasdb
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `actores`
+--
+
+LOCK TABLES `actores` WRITE;
+/*!40000 ALTER TABLE `actores` DISABLE KEYS */;
+INSERT INTO `actores` VALUES (2,'Bradley Cooper','Estados Unidos','1975-01-05'),(3,'Rooney Mara','Estados Unidos','1985-04-17'),(5,'Cate Blanchett','Australia','1969-05-14'),(6,'Toni Collette','Australia','1972-11-01'),(7,'Willem Dafoe','Estados Unidos','1955-07-22'),(8,'David Strathairn','Estados Unidos','1949-01-26'),(9,'Richard Jenkins','Estados Unidos','1947-05-04'),(19,'Sam Worthington','Reino Unido','1976-08-02'),(20,'Zoe Saldana','Estados Unidos','1978-06-19'),(21,'Sigourney Weaver','Estados Unidos','1949-10-08'),(22,'Stephen Lang','Estados Unidos','1952-07-11');
+/*!40000 ALTER TABLE `actores` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-30  2:37:51
