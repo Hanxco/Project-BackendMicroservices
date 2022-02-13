b1890b53eab179df1a5d69fc60a6d5b3907c2bfa
Hanxco <nachohitadg@gmail.com>
2022-02-13 19:50:05 +0100

    eureka server and usuariosCriticas microservices initialized

---
+++ usuariosCriticas/src/main/java/com/nachohm/usuariosCriticas/UsuariosCriticasApplication.java
@@ -0,0 +1,13 @@
+package com.nachohm.usuariosCriticas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariosCriticasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariosCriticasApplication.class, args);
+	}
+
+}
---
+++ usuariosCriticas/src/test/java/com/nachohm/usuariosCriticas/UsuariosCriticasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nachohm.usuariosCriticas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosCriticasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
